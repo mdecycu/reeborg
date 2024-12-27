29804c116903082bf25c1b7d87edb31d17b2a74e
mdecycu <mdecycuorg@gmail.com>
2024-12-27 13:07:13 +0800

    加入與 CoppeliaSim, Webots, Solvespace, NX2312 以及 Leo Editor 有關的開發相關資訊

--- docs/init.py
+++ docs/init.py
@@ -10,10 +10,10 @@ config_dir = _curdir + "/config/"
 class Init(object):
     # uwsgi as static class variable, can be accessed by Init.uwsgi
     uwsgi = False
-    site_title = "cmsimde_site"
+    site_title = "Reeborg"
     ip = "127.0.0.1"
-    dynamic_port = 9443
-    static_port = 8443
+    dynamic_port = 9449
+    static_port = 8449
     def __init__(self):
         # hope to create downloads and images directories　
         if not os.path.isdir(_curdir + "/downloads"):
